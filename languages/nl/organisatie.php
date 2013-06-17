b5a00c6c2f68872edfcdd3b295141ccd1139b62f
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-06-18 00:45:33 +0200

    Organisatie-pagina beter ingedeeld.

--- languages/nl/organisatie.php
+++ languages/nl/organisatie.php
@@ -1,28 +1,46 @@
 <h2>Cover</h2>
 <p>Cover is de studievereniging voor de opleidingen Kunstmatige Intelligentie en Informatica. Als studievereniging organiseren we het hele jaar door veel leuke studiegerelateerde en ontspannende activiteiten. Zo gaan we regelmatig langs bij bedrijven om te kijken hoe het er op de werkvloer aan toe gaat en komen er regelmatig sprekers langs die een interessant verhaal vertellen. Daarnaast hebben we natuurlijk ook een boel ontspannende activiteiten zoals een maandelijkse borrel, spellenavond en vele andere leuke, originele activiteiten waarbij je je studiegenoten op een andere manier leert kennen dan in de collegebank. Ten slotte kun je via ons ook korting krijgen op je boeken.</p>
 <p>Verder heeft Cover ook haar eigen kamer (de SLACK) op de universiteit. Hier kun je tussen de colleges door even rustig bijkomen en gezellig even wat eten, drinken en chillen met je studiegenoten. Ook als je vast zit met opdrachten en dergelijke zijn hier altijd wel wat ouderejaars aanwezig die je even kunnen helpen met je problemen. Hopelijk zullen we jullie hier nog veel terug zien!</p>
+
 <h2>De Commissie</h2> 
-<img src="images/hoofdjes/Commissie.png" class="commissie_foto"/>   
-<p>Hoi, ik ben Annemarie en ik ben 21 jaar. Ik ben derdejaars student KI. Ik heb het afgelopen jaar met veel plezier, samen met de IntroCie, een super leuk introkamp voor jullie georganiseerd. Ik hoop dat jullie het heel erg naar jullie zin gaan hebben en dat we samen een mooie tijd gaan beleven. Ik heb er zin in! Tot dan!</p>
-<img src="images/hoofdjes/Annemarie.png" class="hoofdje_foto"/>
+<img src="images/hoofdjes/Commissie.png" class="foto-commissie">   
 
-<p>Hee daar! Ik ben Hayo, 20 jaar en als jij me ziet op introkamp ben ik alweer derdejaars student Kunstmatige Intelligentie. Voordat ik in deze commissie zat, zat ik bij een commissie die een groot feest gaf voor heel veel studieverenigingen. Ik ben van plan om van dit kamp net zo’n groot feest te maken!</p>
-<img src="images/hoofdjes/Hayo.png" class="hoofdje_foto" />
+<section class="profiel">
+	<img src="images/hoofdjes/Annemarie.png" class="foto-hoofdje">
+	<p>Hoi, ik ben Annemarie en ik ben 21 jaar. Ik ben derdejaars student KI. Ik heb het afgelopen jaar met veel plezier, samen met de IntroCie, een super leuk introkamp voor jullie georganiseerd. Ik hoop dat jullie het heel erg naar jullie zin gaan hebben en dat we samen een mooie tijd gaan beleven. Ik heb er zin in! Tot dan!</p>
+</section>
 
-<p>Hi! Ik ben Marten en ik ben 23 jaar oud. Ik ben inmiddels al vierdejaars KI-student (komend jaar vijfdejaars) en net een half jaartje bezig met mijn master. Dit wordt voor mij de tweede keer dat ik ons geweldige introkamp mag organiseren en ik hoop dat jullie er al net zo veel zin hebben als ik! :)</p>
-<img src="images/hoofdjes/Marten.png" class="hoofdje_foto" />
+<section class="profiel">
+	<img src="images/hoofdjes/Hayo.png" class="foto-hoofdje">
+	<p>Hee daar! Ik ben Hayo, 20 jaar en als jij me ziet op introkamp ben ik alweer derdejaars student Kunstmatige Intelligentie. Voordat ik in deze commissie zat, zat ik bij een commissie die een groot feest gaf voor heel veel studieverenigingen. Ik ben van plan om van dit kamp net zo’n groot feest te maken!</p>
+</section>
 
-<p>Hallo, ik ben Lianne, 20 jaar en ik begin dit jaar aan de studie Human Technology aan de Hanze. Na anderhalf jaar KI te hebben gestudeerd, heb ik helaas besloten hiermee te stoppen. Dit hield me natuurlijk niet tegen om een schitterend kamp voor jullie neer te zetten! Ik hoop jullie allemaal eind augustus te zien.</p>
-<img src="images/hoofdjes/Lianne.png" class="hoofdje_foto" />
+<section class="profiel">
+	<img src="images/hoofdjes/Marten.png" class="foto-hoofdje">
+	<p>Hi! Ik ben Marten en ik ben 23 jaar oud. Ik ben inmiddels al vierdejaars KI-student (komend jaar vijfdejaars) en net een half jaartje bezig met mijn master. Dit wordt voor mij de tweede keer dat ik ons geweldige introkamp mag organiseren en ik hoop dat jullie er al net zo veel zin hebben als ik! :)</p>
+</section>
 
-<p>Hey, ik ben Steven. Ik ben 22 jaar oud en inmiddels 4ejaars student Kunstmatige Intelligentie. Wij hebben als IntroCie (introductiekampcommissie) al menig kamp bijgewoond of zelfs georganiseerd, en elke keer is het een geweldig gezellig en leuk weekend geweest. Ik hoop dan ook dat ik jullie eind augustus allemaal zie op het introkamp!</p>
-<img src="images/hoofdjes/Steven.png" class="hoofdje_foto" />
+<section class="profiel">
+	<img src="images/hoofdjes/Lianne.png" class="foto-hoofdje">
+	<p>Hallo, ik ben Lianne, 20 jaar en ik begin dit jaar aan de studie Human Technology aan de Hanze. Na anderhalf jaar KI te hebben gestudeerd, heb ik helaas besloten hiermee te stoppen. Dit hield me natuurlijk niet tegen om een schitterend kamp voor jullie neer te zetten! Ik hoop jullie allemaal eind augustus te zien.</p>
+</section>
 
-<p>Moi, ik ben Marijn, 20 jaar oud en ik ben eerstejaars KI, aankomend jaar ga ik informatica studeren. Als eerstejaars bij Cover is het ontzettend leuk en daarom heb ik ook besloten het introkamp te organiseren voor jullie! Have Fun!</p>
-<img src="images/hoofdjes/Marijn.png" class="hoofdje_foto" />
+<section class="profiel">
+	<img src="images/hoofdjes/Steven.png" class="foto-hoofdje">
+	<p>Hey, ik ben Steven. Ik ben 22 jaar oud en inmiddels 4ejaars student Kunstmatige Intelligentie. Wij hebben als IntroCie (introductiekampcommissie) al menig kamp bijgewoond of zelfs georganiseerd, en elke keer is het een geweldig gezellig en leuk weekend geweest. Ik hoop dan ook dat ik jullie eind augustus allemaal zie op het introkamp!</p>
+</section>
 
-<p>Ik ben Diederick en een beetje dom</p>
-<img src="images/hoofdjes/Diederick.png" class="hoofdje_foto" />
+<section class="profiel">
+	<img src="images/hoofdjes/Marijn.png" class="foto-hoofdje">
+	<p>Moi, ik ben Marijn, 20 jaar oud en ik ben eerstejaars KI, aankomend jaar ga ik informatica studeren. Als eerstejaars bij Cover is het ontzettend leuk en daarom heb ik ook besloten het introkamp te organiseren voor jullie! Have Fun!</p>
+</section>
 
-<p>Ik ben Floris en een beetje dom</p>
-<img src="images/hoofdjes/Floris.png" class="hoofdje_foto" />
+<section class="profiel">
+	<img src="images/hoofdjes/Diederick.png" class="foto-hoofdje">
+	<p>Ik ben Diederick en een beetje dom</p>
+</section>
+
+<section class="profiel">
+	<img src="images/hoofdjes/Floris.png" class="foto-hoofdje">
+	<p>Ik ben Floris en een beetje dom</p>
+</section>
