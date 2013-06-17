7995ef1e2029b9650ef801b25a97bb2a404d555b
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-06-18 00:46:45 +0200

    Engels gastenboek

--- languages/en/gastenboek.php
+++ languages/en/gastenboek.php
@@ -1,1 +1,26 @@
-<h2>Guestenbook</h2>
+<h2>Guestbook</h2>
+
+<form method="post" action="gastenboek.php">
+	<label class="gastenboek-naam">
+		<span>Name</span>
+		<input type="text" name="naam">
+	</label>
+	
+	<label class="gastenboek-email">
+		<span>Email</span>
+		<input type="text" name="email">
+	</label>
+	
+	<label class="gastenboek-bericht">
+		<span>Message</span>
+		<textarea name="bericht"></textarea>
+	</label>
+
+	<button type="submit">Post message</button>
+</form>
+
+<div class="gastenboek">
+	<!-- wordt extern geladen enzo -->
+</div>
+
+<button class="gastenboek-next-page-button">More messages!</button>
