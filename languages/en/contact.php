7b09b7b45b5699eeefcedf82fe617958a091a2dd
Marijn Pool <info@marijnpool.nl>
2013-06-23 16:10:04 +0200

    contact en kamp bijgewerkt

--- languages/en/contact.php
+++ languages/en/contact.php
@@ -1,2 +1,6 @@
 <h2>Contact!</h2>
-<p>The awesome new and last song from the album Random Access Memory by Daft Punk.</p>
+<p>Should you have any questions or remarks, you should feel free to contact us through our e-mail at introcie@svcover.nl. In emergencies it is always possible to contact the chairman: Steven Warmelink (+31)(0)6-22 61 83 71.</p>
+<ul>
+<li>E-mail: <a href="mailto:introcie@svcover.nl">introcie@svcover.nl</a></li>
+<li>Tel. (emergencies):(+31)(0)6-22 61 83 71</li>
+</ul>
