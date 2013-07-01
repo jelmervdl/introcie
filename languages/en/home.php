5705f8a5fedd2332fecb408d31cb353225482b89
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-07-01 23:47:46 +0200

    M'n Google Analytics-guy belde.. "Schrijf je nu in" is nu een klikbare call-to-action, voor een betere conversie.

--- languages/en/home.php
+++ languages/en/home.php
@@ -2,4 +2,4 @@
 <p>Will you begin your Bachelor Artificial Intelligence or Computing Science and join the corresponding association ‘Cover’? Come with us on our introduction camp! Meet the people you’ll see more often during the classes and get to know eachother a lot better before those classes even start. We, the introduction committee, are proud to be organizing this year’s camp from the 30th of August till the 1st of September.</p>
 
 <p>The camp is themed “How I met my Future Friend”, which can only hold lots of joy.</p>
-<p>Sign up for the camp!</p>
+<p><a href="#inschrijven">Sign up for the camp!</a></p>
--- languages/nl/home.php
+++ languages/nl/home.php
@@ -1,4 +1,4 @@
 <h2>Home!</h2>
 <p>Begin jij dit jaar je studie Kunstmatige Intelligentie of Informatica en kom je gezellig bij de bijbehorende studievereniging Cover? Ga dan mee op ons Introkamp! Ontmoet alvast mensen die je de komende tijd vaker gaat zien en leer elkaar beter kennen. Wij, als introductiecommissie, zijn er trots op dat wij het kamp van dit jaar mogen organiseren van 30 augustus tot en met 1 september.</p>
 <p>Met het thema “How I met my Future Friends” kan het bijna alleen maar een succes worden.</p>
-<p>Schrijf je nu in!</p>
+<p><a href="#inschrijven">Schrijf je nu in!</a></p>
