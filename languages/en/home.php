b86b9c70e0f50d8ff3b33a11c5a17543baa5c069
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-07-01 23:46:48 +0200

    Datums toegevoegd aan home en kamp pagina's

--- languages/en/home.php
+++ languages/en/home.php
@@ -1,5 +1,5 @@
 <h2>Home!</h2>
-<p>Will you begin your Bachelor Artificial Intelligence or Computing Science and join the corresponding association ‘Cover’? Come with us on our introduction camp! Meet the people you’ll see more often during the classes and get to know eachother a lot better before those classes even start. We, the introduction committee, are proud to be organizing this year’s camp.</p>
+<p>Will you begin your Bachelor Artificial Intelligence or Computing Science and join the corresponding association ‘Cover’? Come with us on our introduction camp! Meet the people you’ll see more often during the classes and get to know eachother a lot better before those classes even start. We, the introduction committee, are proud to be organizing this year’s camp from the 30th of August till the 1st of September.</p>
 
 <p>The camp is themed “How I met my Future Friend”, which can only hold lots of joy.</p>
 <p>Sign up for the camp!</p>
